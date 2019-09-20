01451493a3adb9c2947b882a85eab6427932636c
shaoguang <48747588+shaoguangwu@users.noreply.github.com>
2019-09-20 17:26:00 +0800

    update

--- GenericMatrix.h
+++ GenericMatrix.h
@@ -1,9 +1,19 @@
-/*!
- * \file            GenericMatrix.h
- * \brief           The header file of the GenericMatrix class.
- * \date            2019-09-20
- * \author          shaoguang
- * \copyright       MIT License
+/**
+ *  Copyright (C) 2019 Shaoguang. All rights reserved.
+ * 
+ *  GenericMatrix, a generic template matrix class.
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 #ifndef __GENERICMATRIX_H__
@@ -32,7 +42,7 @@ class GenericMatrix
 {
 public:
     using size_type = std::size_t;
-    using value_type = typename Elem;
+    using value_type = Elem;
 
     GenericMatrix();
     GenericMatrix(size_type row, size_type col);
--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # GenericMatrix
-C++ generic matrix class, C++11 Required Support.
+
+C++ generic matrix template class, C++11 Required Support.
+
 C++通用矩阵类设计，需编译器支持C++11.
+
+...cpp
+    GenericMatrix<> matrix(10, 5);
+    std::cout << matrix << std::endl;
+...
+
+The Result is:
+"
+GenericMatrix<10, 5, f>(
+         01         00         00         00         00
+         00         01         00         00         00
+         00         00         01         00         00
+         00         00         00         01         00
+         00         00         00         00         01
+         00         00         00         00         00
+         00         00         00         00         00
+         00         00         00         00         00
+         00         00         00         00         00
+         00         00         00         00         00
+)
+"
